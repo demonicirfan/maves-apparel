2b6cf9651387b0af4c3296199f76e0ec6558c06c
Irfan Asif <hashtag.irfan@gmail.com>
2021-12-31 14:47:16 +0530

    server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const productRouter = require('./routes/Products');
+const cors = require('cors')
 var bodyParser = require('body-parser');
 
 const app = express();
