90e0446f0ca5f33f86ba024b7b7118849d220187
Irfan Asif <hashtag.irfan@gmail.com>
2022-01-04 23:10:05 +0530

    added contact and about

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,12 +1,123 @@
-import { Center, Heading } from '@chakra-ui/react';
-import React from 'react';
+import {
+  Box,
+  Container,
+  SimpleGrid,
+  Icon,
+  Text,
+  Stack,
+  HStack,
+  VStack,
+  Center,
+  Avatar,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
-const About = () => {
+import { CheckIcon } from '@chakra-ui/icons';
+
+// Replace test data with your own
+const features = [
+  {
+    id: 'hii',
+    title: 'Lorem ipsum dolor sit amet',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',
+  },
+  {
+    id: 'hii',
+    title: 'Lorem ipsum dolor sit amet',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',
+  },
+  {
+    id: 'hii',
+    title: 'Lorem ipsum dolor sit amet',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',
+  },
+];
+
+export default function Aboutus() {
+  const navigate = useNavigate();
+
   return (
-    <Center py={'80'} minH={'90vh'}>
-      <Heading>About US Page</Heading>
-    </Center>
-  );
-};
+    <Box p={4} my={['24', '32']}>
+      <Stack
+        spacing={4}
+        as={Container}
+        maxW={'3xl'}
+        textAlign={'center'}
+        mb={'16'}
+      >
+        <Text fontSize={['3xl', '4xl']} fontFamily={'Italiana'}>
+          This is the headline
+        </Text>
+        <Text color={'gray.600'} fontSize={['md', 'lg']} fontWeight={'200'}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua.
+        </Text>
+        <Center
+          as={'button'}
+          bg={'gray.900'}
+          px={'4'}
+          py={'2'}
+          onClick={() => {
+            navigate('contact-us');
+          }}
+          _hover={{
+            backgroundColor: 'transparent',
+            color: 'black',
+            outline: '2px solid black',
+          }}
+          w={'fit-content'}
+          alignSelf={'center'}
+          color={'white'}
+        >
+          <Text>Contact Us</Text>
+        </Center>
+      </Stack>
 
-export default About;
+      <Container maxW={'6xl'} mt={10}>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10}>
+          {features.map((feature) => (
+            <HStack key={feature.id} align={'top'} alignSelf={'center'}>
+              <Box color={'green.400'} px={2}>
+                <Icon as={CheckIcon} />
+              </Box>
+              <VStack align={'start'}>
+                <Text fontWeight={600}>{feature.title}</Text>
+                <Text color={'gray.600'}>{feature.text}</Text>
+              </VStack>
+            </HStack>
+          ))}
+        </SimpleGrid>
+        <Stack
+          pt={['16', '24']}
+          spacing={4}
+          as={Container}
+          maxW={'3xl'}
+          textAlign={'center'}
+        >
+          <Avatar
+            alignSelf={'center'}
+            source={{
+              uri: 'https://pbs.twimg.com/profile_images/1369921787568422915/hoyvrUpc_400x400.jpg',
+            }}
+            size={'xl'}
+          ></Avatar>
+
+          <Text color={'gray.600'} fontSize={['md', 'lg']} fontWeight={'200'}>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </Text>
+          <Center flexDirection={'column'} gap={'2'}>
+            <Text alignSelf={'center'} fontSize={['lg', 'xl']}>
+              Name of CEO
+            </Text>
+            <Text alignSelf={'center'} fontSize={['xs', 'sm']}>
+              CEO
+            </Text>
+          </Center>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
--- client/src/Pages/ContactUs.js
+++ client/src/Pages/ContactUs.js
@@ -1,12 +1,155 @@
-import { Center, Heading } from '@chakra-ui/react';
-import React from 'react';
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Icon,
+  Button,
+  VStack,
+  HStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+} from '@chakra-ui/react';
+import { MdPhone, MdEmail, MdLocationOn, MdOutlineEmail } from 'react-icons/md';
+import { BsPerson } from 'react-icons/bs';
 
-const Contact = () => {
+export default function contact() {
   return (
-    <Center py={'80'} minH={'90vh'}>
-      <Heading>Contact US Page</Heading>
-    </Center>
+    <Container
+      bg='white'
+      maxW='full'
+      pt={16}
+      centerContent
+      overflow='hidden'
+      minH={'95vh'}
+    >
+      <Flex my={'auto'}>
+        <Box
+          bg='gray.900'
+          color='white'
+          m={{ sm: 0, md: 16, lg: 10 }}
+          p={{ sm: 5, md: 5, lg: 16 }}
+        >
+          <Box p={4}>
+            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+              <WrapItem>
+                <VStack alignItems={['center', 'flex-start']} spacing={'8'}>
+                  <Box>
+                    <Heading fontFamily={'Italiana'} fontSize={['2xl', '3xl']}>
+                      Contact
+                    </Heading>
+                    <Text color='gray.500' fontSize={['sm', 'md', 'md']}>
+                      Fill up the form to contact
+                    </Text>
+                  </Box>
+                  <VStack
+                    w={'full'}
+                    alignItems={['start', 'start']}
+                    spacing={[2, 3]}
+                  >
+                    <Button
+                      size='md'
+                      height='48px'
+                      width='fit-content'
+                      variant='ghost'
+                      color='#FAF9F8'
+                      fontWeight={'400'}
+                      textAlign={'start'}
+                      _hover={{ outline: '2px solid #FAF9F8' }}
+                      leftIcon={<MdPhone color='#FAF9F8' size='20px' />}
+                    >
+                      +91-988888888
+                    </Button>
+                    <Button
+                      size='md'
+                      height='48px'
+                      width='fit-content'
+                      variant='ghost'
+                      fontWeight={'400'}
+                      color='#FAF9F8'
+                      _hover={{ outline: '2px solid #FAF9F8' }}
+                      leftIcon={<MdEmail color='#FAF9F8' size='20px' />}
+                    >
+                      info@mavesapparel.com
+                    </Button>
+                  </VStack>
+                  <HStack align={'flex-start'} px={['2', '4']} w={'full'}>
+                    <Icon w='30px' h='30px'>
+                      <MdLocationOn color='#FAF9F8' size='20px' />
+                    </Icon>
+                    <VStack spacing={2} alignItems='flex-start'>
+                      <Text>71-75 Shelton Street,</Text>
+                      <Text>Covent Garden,</Text>
+                      <Text>London</Text>
+                      <Text>WC2H 9JQ</Text>
+                      <Text>United Kingdom</Text>
+                    </VStack>
+                  </HStack>
+                </VStack>
+              </WrapItem>
+              <WrapItem>
+                <Box bg='white' borderRadius='lg'>
+                  <Box m={8} color='#0B0E3F'>
+                    <VStack spacing={5}>
+                      <FormControl id='name'>
+                        <FormLabel>Your Name</FormLabel>
+                        <InputGroup borderColor='#E0E1E7'>
+                          <InputLeftElement
+                            pointerEvents='none'
+                            children={<BsPerson color='gray.800' />}
+                          />
+                          <Input type='text' size='md' />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id='name'>
+                        <FormLabel>Mail</FormLabel>
+                        <InputGroup borderColor='#E0E1E7'>
+                          <InputLeftElement
+                            pointerEvents='none'
+                            children={<MdOutlineEmail color='gray.800' />}
+                          />
+                          <Input type='text' size='md' />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id='name'>
+                        <FormLabel>Message</FormLabel>
+                        <Textarea
+                          borderColor='gray.300'
+                          _hover={{
+                            borderRadius: 'gray.300',
+                          }}
+                          placeholder='message'
+                        />
+                      </FormControl>
+                      <FormControl id='name' float='right'>
+                        <Button
+                          rounded={'0'}
+                          variant='solid'
+                          bg='gray.900'
+                          color='white'
+                          fontWeight={'400'}
+                          _hover={{
+                            backgroundColor: 'gray.600',
+                          }}
+                        >
+                          Send Message
+                        </Button>
+                      </FormControl>
+                    </VStack>
+                  </Box>
+                </Box>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
   );
-};
-
-export default Contact;
+}
